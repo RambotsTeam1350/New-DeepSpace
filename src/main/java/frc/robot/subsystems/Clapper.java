4587af04be183db8d08946b9357b3ae6336fea87
pcosta19 <32554433+pcosta19@users.noreply.github.com>
2019-01-18 11:33:30 -0500

    Updated 1/18/19
    Makes the clapper differential drive

--- src/main/java/frc/robot/subsystems/Clapper.java
+++ src/main/java/frc/robot/subsystems/Clapper.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
 public class Clapper extends Subsystem {
@@ -19,30 +20,34 @@ public class Clapper extends Subsystem {
 	
 	//declaring the motor controllers, VictorSP is the type of motor controller we use
 	//be sure to import your ClapperCommands, see above
-	private VictorSP clapperMotorController;
+	private VictorSP clapperMotorControllerLeft;
+	private VictorSP clapperMotorControllerRight;
+	private static DifferentialDrive clapperMotors;
 	private ClapperCommands clapperInstance;
 	
 	//runs the clapper
-	public void initDefaultCommand() 
-	{
+	public void initDefaultCommand() {
 		setDefaultCommand(clapperInstance);
 		//clapperInstance is defined below and is an instance of ClapperCommands
 	}
 	
-   	private static Clapper instance;
+    private static Clapper instance;
 
-	//creates Clapper instance
+	//creates Clapper subsystem instance
 	public static Clapper getInstance()
 	{
 		if(instance == null)
 			instance = new Clapper();
 		return instance;
-	}
-    
-    public void moveClapperMotor(double speed)
-	{
-       	clapperMotorController.set(speed);
-   	}
+    }
+	
+	//clapperMotors acts as arcade drive but with a fixed rotation of 0
+	//this makes it so we can control moving the motors with just one stick
+	//and the rotation never changes from 0 because we don't need it to
+	//"true" sets squared inputs to true
+    public void moveClapperMotors(double speed){
+        clapperMotors.arcadeDrive(speed, 0, true);
+    }
 	
 	
 	//constructs clapper (nothing needed inside)
@@ -53,11 +58,16 @@ public class Clapper extends Subsystem {
 
 	public void initialize()
 	{
+		
 		//the clapperInstance is the instance of the ClapperCommands
 		clapperInstance = ClapperCommands.getInstance();
 		
-		//makes it so the instance of the clapperMotorController in this class
-		//is the same as the clapperMotorContoller and its port in the robotmap class
-		clapperMotorController = new VictorSP(RobotMap.clapperMotor);
+		//connects these to the pwm's
+		clapperMotorControllerLeft = new VictorSP(RobotMap.clapperMotorLeft);
+		clapperMotorControllerRight = new VictorSP(RobotMap.clapperMotorRight);
+
+		//constructs clapperMotors
+		clapperMotors = new DifferentialDrive(clapperMotorControllerLeft, clapperMotorControllerRight);
+
 	}
 }
